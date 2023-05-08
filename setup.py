2d1ea32c61e491eeb382614527c1541e593d09f1
Matthew Bachtell <126006749+mbachtell-nais@users.noreply.github.com>
2023-05-08 08:13:44 -0400

    rolled back to just 3.10 support

--- setup.py
+++ setup.py
@@ -78,8 +78,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     python_requires=">=3.8",
@@ -99,7 +97,7 @@ setup(
         "requests>=2.18.4",
         # This is necessary to support the python_requires kwarg
         "setuptools >= 24.2.0",
-        "sslyze>=3.0.0,<5.1.4",
+        "sslyze>=3.0.0,<5.0.0",
         "wget>=3.2",
     ],
     extras_require={
